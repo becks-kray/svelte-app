c8b36edb457c996f401ca37741808c3800504dc4
Kray Freestone <kray.freestone@beckshybrids.com>
2023-03-31 15:12:12 -0400

    added tailwind and maplibre map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="items-center">
+	<h1 class="text-3xl font-bold max-w-sm p-6 mx-auto shadow-lg">Nothing on this page</h1>
+</div>
+
+<style lang="postcss">
+	/* :global(html) {
+		background-color: theme(colors.blue.300);
+	} */
+</style>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	import maplibre from 'maplibre-gl';
+
+	onMount(() => {
+		new maplibre.Map({
+			container: 'map', // container id
+			style: 'https://demotiles.maplibre.org/style.json', // style URL
+			center: [0, 0], // starting position [lng, lat]
+			zoom: 1 // starting zoom
+		});
+	});
+</script>
+
+<div class="p-0 m-0">
+    <div class="absolute top-0 bottom-0 w-full" id="map"></div>
+</div>
